7389a62280ff7ec822edd675dd62e76aae9d8388
Fabricio Migues <fabricio.migues@gmail.com>
2018-05-22 22:46:20 +0200

    Model Planet with methods initalizae and put_obstacle created

---
+++ app/planet.rb
@@ -0,0 +1,14 @@
+class Planet
+  attr_reader :planet_max_x, :planet_max_y
+  attr_accessor :obstacle, :obstacle_x, :obstacle_y
+
+  def initialize(planet_max_x, planet_max_y)
+    @planet_max_x = planet_max_x
+    @planet_max_y = planet_max_y
+    @obstacle = nil
+  end
+
+  def put_obstacle(obstacle_x, obstacle_y)
+    @obstacle = (obstacle_x, obstacle_y)
+  end
+end
